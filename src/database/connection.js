6913da6feec9cc255f8de9f15c0f9126f2d8f691
Lavarda <vitorlavarda.souza@gmail.com>
2020-12-16 18:45:02 -0300

    Testing deploy

--- src/database/connection.js
+++ src/database/connection.js
@@ -8,26 +8,26 @@ const databaseConfig = require('./config/config.js')
 const db = {}
 
 const connection = new Sequelize(databaseConfig);
-const modelsDirectory = path.resolve(__dirname, '../', 'models')
+// const modelsDirectory = path.resolve(__dirname, '../', 'models')
 
-fs
-  .readdirSync(modelsDirectory)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(modelsDirectory, file));
-    model.init(connection)
-    connection[model.name] = model;
-  });
+// fs
+//   .readdirSync(modelsDirectory)
+//   .filter(file => {
+//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+//   })
+//   .forEach(file => {
+//     const model = require(path.join(modelsDirectory, file));
+//     model.init(connection)
+//     connection[model.name] = model;
+//   });
 
-Object.keys(connection).forEach(modelName => {
-  if (connection[modelName].associate) {
-    connection[modelName].associate(connection);
-  }
-});
+// Object.keys(connection).forEach(modelName => {
+//   if (connection[modelName].associate) {
+//     connection[modelName].associate(connection);
+//   }
+// });
 
-db.sequelize = connection;
-db.Sequelize = Sequelize;
+// db.sequelize = connection;
+// db.Sequelize = Sequelize;
 
 module.exports = connection
